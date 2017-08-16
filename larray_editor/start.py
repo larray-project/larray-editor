6c4f6c4e5302c7caf190ea513dd972496cdf4bee
Alix Damman <ald@plan.be>
2017-08-16 11:34:38 +0200

    fixed import in start.py: imports edit from larray_editor instead of larray

--- larray_editor/start.py
+++ larray_editor/start.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import sys
-from larray import edit
+from larray_editor import edit
 
 def main():
     if os.name == 'nt':
