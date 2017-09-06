2e03e320593efe2e9fb0898f7ad5626b8c467d0e
Gaëtan de Menten <gdementen@gmail.com>
2017-09-06 14:53:44 +0200

    prepare for 0.25.2 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray-editor'
-VERSION = '0.25.1'
+VERSION = '0.25.2'
 AUTHOR = 'Gaetan de Menten, Geert Bryon, Johan Duyck, Alix Damman'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "Graphical User Interface for LArray library"
