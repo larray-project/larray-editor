69848415b053ca0335df1e72e1ed800a0a7e5fd4
Gaëtan de Menten <gdementen@gmail.com>
2017-11-29 11:56:32 +0100

    allow start.py to be run standalone

--- larray_editor/start.py
+++ larray_editor/start.py
@@ -4,8 +4,13 @@ import os
 import sys
 from larray_editor import edit
 
+
 def main():
     if os.name == 'nt':
         sys.stdout = open(os.devnull, "w")
         sys.stderr = open(os.path.join(os.getenv("TEMP"), "stderr-" + os.path.basename(sys.argv[0])), "w")
     edit()
+
+
+if __name__ == '__main__':
+    main()
