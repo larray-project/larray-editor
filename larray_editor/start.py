6d664e0ba74738518977ed90ea4baf25402b6f44
Gaëtan de Menten <gdementen@gmail.com>
2017-12-07 15:28:42 +0100

    allow passing a file path as command line argument to larray-editor to open it e.g. C:\Users\gdm>larray-editor c:\tmp\test.csv

--- larray_editor/start.py
+++ larray_editor/start.py
@@ -9,7 +9,7 @@ def main():
     if os.name == 'nt':
         sys.stdout = open(os.devnull, "w")
         sys.stderr = open(os.path.join(os.getenv("TEMP"), "stderr-" + os.path.basename(sys.argv[0])), "w")
-    edit()
+    edit(*sys.argv[1:])
 
 
 if __name__ == '__main__':
