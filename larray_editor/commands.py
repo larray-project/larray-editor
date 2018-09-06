036e1a228bde78bc3b4a37d6bba6960293fd7fe5
Gaëtan de Menten <gdementen@gmail.com>
2018-09-06 16:39:51 +0200

    fix #157: edit(single_array) did not allow to change the array values

--- larray_editor/commands.py
+++ larray_editor/commands.py
@@ -35,36 +35,82 @@ class EditArrayCommand(QUndoCommand):
     ----------
     editor: MappingEditor
         Instance of MappingEditor
-    array_name : str
-        name of array to edit
+    target : object
+        target array to edit. Can be given under any form.
     changes: (list of) instance(s) of ArrayValueChange
         List of changes
     """
 
-    def __init__(self, editor, array_name, changes):
+    def __init__(self, editor, target, changes):
         QUndoCommand.__init__(self)
         self.editor = editor
-        self.array_name = array_name
-        if not isinstance(changes, (list, tuple)):
-            changes = (changes,)
+        self.target = target
+        assert isinstance(changes, list)
         self.changes = changes
 
-        if len(changes) == 1:
-            text_command = "Editing Cell {} of {}".format(changes[0].key, array_name)
-        else:
-            text_command = "Pasting {} Cells in {}".format(len(changes), array_name)
+        text_command = self.get_description(target, changes)
         self.setText(text_command)
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug("Edit command pushed: {}".format(text_command))
 
     def undo(self):
         for change in self.changes:
-            command = "{}[{}] = {}".format(self.array_name, change.key, change.old_value)
-            self.editor.kernel.shell.run_cell(command)
+            self.apply_change(change.key, change.old_value)
         self.editor.arraywidget.model_data.reset()
 
     def redo(self):
         for change in self.changes:
-            command = "{}[{}] = {}".format(self.array_name, change.key, change.new_value)
-            self.editor.kernel.shell.run_cell(command)
+            self.apply_change(change.key, change.new_value)
         self.editor.arraywidget.model_data.reset()
+
+    def get_description(self, target, changes):
+        raise NotImplementedError()
+
+    def apply_change(self, key, new_value):
+        raise NotImplementedError()
+
+
+class EditSessionArrayCommand(EditArrayCommand):
+    """
+    Class representing the change of one or several value(s) of an array.
+
+    Parameters
+    ----------
+    editor: MappingEditor
+        Instance of MappingEditor
+    target : str
+        name of array to edit
+    changes: (list of) instance(s) of ArrayValueChange
+        List of changes
+    """
+    def get_description(self, target, changes):
+        if len(changes) == 1:
+            return "Editing Cell {} of {}".format(changes[0].key, target)
+        else:
+            return "Pasting {} Cells in {}".format(len(changes), target)
+
+    def apply_change(self, key, new_value):
+        self.editor.kernel.shell.run_cell("{}[{}] = {}".format(self.target, key, new_value))
+
+
+class EditCurrentArrayCommand(EditArrayCommand):
+    """
+    Class representing the change of one or several value(s) of the current array.
+
+    Parameters
+    ----------
+    editor : ArrayEditor
+        Instance of ArrayEditor
+    target : LArray
+        array to edit
+    changes : (list of) instance(s) of ArrayValueChange
+        List of changes
+    """
+    def get_description(self, target, changes):
+        if len(changes) == 1:
+            return "Editing Cell {}".format(changes[0].key)
+        else:
+            return "Pasting {} Cells".format(len(changes))
+
+    def apply_change(self, key, new_value):
+        self.target[key] = new_value
