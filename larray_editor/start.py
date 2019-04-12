b7d5d89d62c53930975aea50db2680c6ed04e5e3
Alix Damman <ald@plan.be>
2019-04-12 10:19:38 +0200

    simplified main() in start.py assuming it is very unlikely that someone will one day pass enough arguments to edit() via command-line to have a value for display_caller_info via position in addition to via the keyword

--- larray_editor/start.py
+++ larray_editor/start.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import sys
-import inspect
 from larray_editor import edit
 
 
@@ -10,15 +9,7 @@ def main():
     if os.name == 'nt':
         sys.stdout = open(os.devnull, "w")
         sys.stderr = open(os.path.join(os.getenv("TEMP"), "stderr-" + os.path.basename(sys.argv[0])), "w")
-    try:
-        pos_arg = inspect.getfullargspec(edit).args.index('display_caller_info')
-        if len(sys.argv) > pos_arg + 1:
-            sys.argv[pos_arg + 1] = False
-            edit(*sys.argv[1:])
-        else:
-            edit(*sys.argv[1:], display_caller_info=False)
-    except ValueError:
-        edit(*sys.argv[1:])
+    edit(*sys.argv[1:], display_caller_info=False)
 
 
 if __name__ == '__main__':
