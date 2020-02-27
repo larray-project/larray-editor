d1730c0a3cd28aa8c84078b8b14eafd5c3e117b2
Alix Damman <ald@plan.be>
2020-02-27 15:36:47 +0100

    bump version to 0.33-dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray-editor'
-VERSION = '0.32.1'
+VERSION = '0.33-dev'
 AUTHOR = 'Gaetan de Menten, Geert Bryon, Johan Duyck, Alix Damman'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "Graphical User Interface for LArray library"
