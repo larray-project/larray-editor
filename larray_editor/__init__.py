cc207b61eb4d960d3b17aee51894a1aca2852ce7
Alix Damman <ald@plan.be>
2020-02-27 15:38:40 +0100

    fix #132 : dropped Python 2 support

--- larray_editor/__init__.py
+++ larray_editor/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 from larray_editor.api import *
 
 __version__ = '0.33-dev'
--- larray_editor/arraymodel.py
+++ larray_editor/arraymodel.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 from os.path import basename
 import logging
 from inspect import stack
