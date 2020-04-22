bf26972ec8587ab3e52b74a4280ad89e2deedf80
Gaëtan de Menten <gdementen@gmail.com>
2020-04-22 17:32:08 +0200

    modernize conda recipe

--- condarecipe/larray-editor/build.sh
+++ condarecipe/larray-editor/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-$PYTHON -m pip install . --no-deps --ignore-installed -vvv
+# Ref: https://github.com/ContinuumIO/menuinst/wiki/Menus-in-Conda-Recipes
+# See https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html
+#     for a list of environment variables that are set during the build process.
 
-# Add more build steps here, if they are necessary.
+#mkdir -p "$PREFIX/Menu"
+#if [ $OSX_ARCH ]
+#then
+#    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+#    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+#else
+#    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+#    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+#fi
 
-# See
-# https://conda.io/docs/user-guide/tasks/build-packages/environment-variables.html
-# for a list of environment variables that are set during the build process.
+"$PYTHON" -m pip install . --no-deps -vv || exit 1
