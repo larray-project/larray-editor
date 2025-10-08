66472b3ca2c35e0f4d8651ec0a354b747e9f10e3
Gaëtan de Menten <gdementen@gmail.com>
2025-10-08 17:56:32 +0200

    FEAT/FIX: added usage when using the editor as a command-line program
    also avoids a polluted namespace in the console because we do not pass a depth nor object

--- larray_editor/start.py
+++ larray_editor/start.py
@@ -4,19 +4,29 @@ import sys
 from larray_editor.api import _show_dialog, create_edit_dialog
 
 
-def call_edit():
-    _show_dialog("Viewer", create_edit_dialog, *sys.argv[1:], display_caller_info=False, add_larray_functions=True)
+def call_edit(obj):
+    # we do not use edit() so that we can have display_caller_info=False
+    _show_dialog("Viewer", create_edit_dialog, obj=obj,
+                 display_caller_info=False, add_larray_functions=True)
 
 
 def main():
+    args = sys.argv[1:]
+    if len(args) > 1:
+        print(f"Usage: {sys.argv[0]} [file_path]")
+        sys.exit()
+    elif len(args) == 1:
+        obj = args[0]
+    else:
+        obj = {}
     if os.name == 'nt':
         stderr_path = os.path.join(os.getenv("TEMP"), "stderr-" + os.path.basename(sys.argv[0]))
         with open(os.devnull, "w") as out, open(stderr_path, "w") as err:
             sys.stdout = out
             sys.stderr = err
-            call_edit()
+            call_edit(obj)
     else:
-        call_edit()
+        call_edit(obj)
 
 
 if __name__ == '__main__':
