12a3a0f41fcf27e96cc6ff54e0b680d7a2ce0ee4
Gaëtan de Menten <gdementen@gmail.com>
2026-03-17 12:18:38 +0100

    FIX: refresh objects when setting any attribute(subset) (fixes #310)
    most prominently `df.loc[key] = value` and `df.iloc[key] = value` but
    also `arr.data[0] = -1` or `arr.data = np.array([[5, 6, 7], [8, 9, 0]])`

--- larray_editor/tests/test_inplace_modification_pattern.py
+++ larray_editor/tests/test_inplace_modification_pattern.py
@@ -1,32 +1,51 @@
-from larray_editor.editor import SUBSET_UPDATE_PATTERN
+from larray_editor.editor import UPDATE_VARIABLE_PATTERN
 
 
 def test_pattern():
-    assert SUBSET_UPDATE_PATTERN.match('arr1[1] = 2')
-    assert SUBSET_UPDATE_PATTERN.match('arr1[1]= 2')
-    assert SUBSET_UPDATE_PATTERN.match('arr1[1]=2')
-    assert SUBSET_UPDATE_PATTERN.match("arr1['a'] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[func(mapping['a'])] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1.i[0, 0] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1.iflat[0, 0] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1.points[0, 0] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1.ipoints[0, 0] = arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] += arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] -= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] *= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] /= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] %= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] //= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] **= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] &= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] |= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] ^= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] >>= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0] <<= arr2")
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0]") is None
-    assert SUBSET_UPDATE_PATTERN.match("arr1.method()") is None
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0].method()") is None
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0].method(arg=thing)") is None
-    assert SUBSET_UPDATE_PATTERN.match("arr1[0].method(arg==thing)") is None
-    # this test fails but I don't think it is possible to fix it with regex
-    # assert SUBSET_UPDATE_PATTERN.match("arr1[func('[]=0')].method()") is None
+    matching_patterns = [
+        'arr1[1] = 2',
+        'arr1[1]= 2',
+        'arr1[1]=2',
+        "arr1['a'] = arr2",
+        "arr1[func(mapping['a'])] = arr2",
+        "arr1.i[0, 0] = arr2",
+        "arr1.iflat[0, 0] = arr2",
+        "arr1.points[0, 0] = arr2",
+        "arr1.ipoints[0, 0] = arr2",
+        "arr1[0] += arr2",
+        "arr1[0] -= arr2",
+        "arr1[0] *= arr2",
+        "arr1[0] /= arr2",
+        "arr1[0] %= arr2",
+        "arr1[0] //= arr2",
+        "arr1[0] **= arr2",
+        "arr1[0] &= arr2",
+        "arr1[0] |= arr2",
+        "arr1[0] ^= arr2",
+        "arr1[0] >>= arr2",
+        "arr1[0] <<= arr2",
+        "arr1.data[1] = 2",
+        "arr1.data = np.array([1, 2, 3])"
+    ]
+    for pattern in matching_patterns:
+        match = UPDATE_VARIABLE_PATTERN.match(pattern)
+        assert match is not None and match.group('variable') == 'arr1'
+
+    for pattern in [
+        "df.loc[1] = 2",
+        "df.iloc[1] = 2"
+    ]:
+        match = UPDATE_VARIABLE_PATTERN.match(pattern)
+        assert match is not None and match.group('variable') == 'df'
+
+    # no match
+    for pattern in [
+        "arr1[0]",
+        "arr1.method()",
+        "arr1[0].method()",
+        "arr1[0].method(arg=thing)",
+        "arr1[0].method(arg==thing)",
+        # this test fails but I don't think it is possible to fix it with regex
+        # "arr1[func('[]=0')].method()"
+    ]:
+        assert UPDATE_VARIABLE_PATTERN.match(pattern) is None
